800a7dee04ad50b918aef76788442090cc6cb702
Việt Anh <vivetanh@gmail.com>
2025-12-01 12:24:56 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local base = "https://raw.githubusercontent.com/<username>/<repo>/main/"
+
+local function loadModule(path)
+    local code = HttpService:GetAsync(base .. path)
+    return loadstring(code)()
+end
+
+local menuUI = loadModule("menuUI/menuUI.lua")
+
+-- khi player vào game
+Players.PlayerAdded:Connect(function(player)
+    menuUI.createUI(player)
+end)
 
